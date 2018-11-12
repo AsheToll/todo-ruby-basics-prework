43f5ca5dd8867662a40d3162be32ca94e8faf17a
Ashley Tolen <tolenashley7@gmail.com>
2018-11-12 21:34:22 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,21 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(phrase)
+  phrase
 end
 
-def greeting
-
+def greeting(greeting, name)
+  puts "#{greeting}#{name}"
 end
 
-def return_a_value
-
+def return_a_value(value)
+  puts value
 end
 
 def last_evaluated_value
